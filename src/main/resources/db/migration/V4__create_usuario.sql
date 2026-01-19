d31b043129c1445931cd3c2fb3cdbd3edffaeeab
Ralf Domingues <ralfirandomingues@gmail.com>
2026-01-19 11:47:26 -0300

    Estado atual do projeto (overwrite do remoto)

--- src/main/resources/db/migration/V4__create_usuario.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuario
-(
-    id     BIGSERIAL PRIMARY KEY,
-    nome   VARCHAR(100) NOT NULL,
-    email  VARCHAR(150) NOT NULL UNIQUE,
-    senha  VARCHAR(255) NOT NULL,
-    perfil VARCHAR(20)  NOT NULL,
-    ativo  BOOLEAN      NOT NULL DEFAULT TRUE
-);
